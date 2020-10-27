52a36075f558a7c6c7617beb4731094fac989bc3
Divyakag <divyakag26@gmail.com>
2020-10-27 17:13:17 +0530

    php-consonant-vovel

---
+++ php-constant-vovel.php
@@ -0,0 +1,11 @@
+<?php
+ $x='b';
+ if($x == 'a'||$x == 'e'||$x == 'i'||$x == 'o'||$x == 'u'||$x == 'A'||$x == 'E'||$x == 'I'||$x == 'O'||$x == 'U') 
+ {
+     echo "<h3 style='color:red;'> Vovel </h3>";
+    }
+        else 
+        {
+            echo "<h3 style= 'color:blue'> Consonant </h3>";
+        }
+?>
